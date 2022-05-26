014255b12f887bbcb4d199eacc3ceaf4ded6de6f
Ahmet Can DERİCİOĞLU <100198364+AmtCan@users.noreply.github.com>
2022-05-26 23:54:17 +0300

    cards page
    some visual updates made

--- Cards.php
+++ Cards.php
@@ -1,5 +1,36 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <?php session_start();?>
+<?php
+  $servername = "localhost";
+  $dbname = "car_rental";
+  $password = "";
+  $username = "root";
+  $idofuser = $_SESSION["userid"];
+  $card_array = [];
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  $sql = "SELECT *
+          FROM cards
+          WHERE userid = '$idofuser'";
+  $result = $conn->query($sql);
+  while ($row = $result->fetch_assoc()) {
+    $card_array[]= $row['cardnumber'];
+  }
+  if (count($card_array) == 0) {
+  }
+  else {
+      foreach ($card_array as $value) {
+         if (isset($_POST["$value"])){
+           $sql = "DELETE FROM cards WHERE  cardnumber = '$value'";
+           if ($conn->query($sql) === TRUE) {
+           } else {
+           }
+           header("location:Cards.php");
+           }
+        }
+    }
+
+
+  ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -79,7 +110,7 @@
       .container p{
         margin-left: 20px;
         margin-top: 60px;
-        background-color: rgba(44, 36, 3, 0.5);
+        background-color: brown;
         width: 220px;
         height: 50px;
         font-family: monospace;
@@ -117,40 +148,35 @@
     </style>
   </head>
   <body>
-    <?php
-      $servername = "localhost";
-      $dbname = "car_rental";
-      $password = "";
-      $username = "root";
-      $idofuser = $_SESSION["userid"];
-      $conn = mysqli_connect($servername, $username, $password, $dbname);
-      $sql = "SELECT *
-              FROM cards
-              WHERE userid = '$idofuser'";
-      $result = $conn->query($sql);
-      while ($row = $result->fetch_assoc()) {
-        $card_array[]= $row['cardnumber'];
-      }
-      ?>
+
     <div class="topnav">
         <a class="fa fa-home" onclick="offAll();" href="mainPage.php"> Home</a>
       </div>
-      <div class="container" style="position: absolute; left: 50%; top: 200px; margin-left:-175px;">
-        <div style="">
-          <h1 style="font-size:40px; color:#CEDDD5; font-family: monospace;">Cards</h1>
-        </div>
-        <div id="card-con">
+      <form class="" action="#" method="post">
+        <div class="container" style="position: absolute; left: 50%; top: 200px; margin-left:-175px;">
+          <div style="">
+            <h1 style="font-size:40px; color:#CEDDD5; font-family: monospace;">Cards</h1>
+          </div>
+          <div id="card-con">
 
+          </div>
         </div>
-      </div>
+      </form>
       <script>
 
         var cardarray = <?php echo json_encode($card_array); ?>;
         for (var i = 0; i < cardarray.length; i++) {
           var tag = document.createElement("p");
           var info = document.createTextNode(cardarray[i]);
+          var close = document.createElement("input");
+          close.classList.add("closeicon");
+          close.type = "submit";
+          close.name = cardarray[i];
+          close.value = "X";
           tag.appendChild(info);
+          tag.appendChild(close);
           var element = document.getElementById("card-con");
+
           element.appendChild(tag);
           }
       </script>
